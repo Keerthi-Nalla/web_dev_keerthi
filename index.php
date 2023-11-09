a0dd566996695dc7eda72778bd92dff9da20ddc0
Keerthi-Nalla <nallakeerthipriyayadav@gmail.com>
2023-11-09 22:01:32 +0530

    add responsiveness..

--- index.php
+++ index.php
@@ -105,11 +105,14 @@ input[type="checkbox"] {
 
 
     </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.7.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script>
         function redirectToLogin() {
             window.location.href = "login.php";
         }
     </script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.7.0/dist/js/bootstrap.min.js"></script>
 </head>
 <body>
     <div class="container">
